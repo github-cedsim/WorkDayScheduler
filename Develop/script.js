70437c073a232402eaf32307a1ea5d270dd7596e
Cedric Simpkins <126218396+github-cedsim@users.noreply.github.com>
2024-02-29 10:46:55 -0600

    Add user imput to local storage and current date

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,42 @@
 $(function () {
-
-    // Save button click event listener
-      $(".saveBtn").on("click", function () {
-      let text = $(this).siblings(".description").val();
-      let time = $(this).parent().attr("id");
-      localStorage.setItem(time, text);
-    });
-  
-    // Past, present, future function to change color of time blocks
-      function workTracker() {
-  
-      let currentHour = dayjs().hour();
-      $(".time-block").each(function () {
-        let blockHour = parseInt($(this).attr("id").split("-")[1]);
-        if (blockHour < currentHour) {
-          $(this).addClass("past");
-        } else if (blockHour === currentHour) {
-          $(this).addClass("present")
-        } else {
-          $(this).addClass("future")
-        }
-      });
-    }
-  
-    
+  // Save button click event listener
+  $(".saveBtn").on("click", function () {
+    let text = $(this).siblings(".description").val();
+    let time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
   });
 
+  // Past, present, future function to change color of time blocks
+  function workTracker() {
+    let currentHour = dayjs().hour();
+    $(".time-block").each(function () {
+      let blockHour = parseInt($(this).attr("id").split("-")[1]);
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  // Save user input to local storage and display on page refresh
+
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+  // Display current day at top of calendar
+
+  let currentDay = dayjs().format("dddd, MMMM DD");
+  $("#currentDay").text(currentDay);
+
+  workTracker();
+});
